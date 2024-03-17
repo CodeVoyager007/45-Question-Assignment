bf24315b5145c0bc885377a3fc8e56099f38bcd0
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-17 17:02:45 +0500

    Add files via upload

---
+++ Question7/Q7.js
@@ -0,0 +1,12 @@
+// Addition operation
+var additionResult = 5 + 3;
+console.log("5 + 3 = ".concat(additionResult));
+// Subtraction operation
+var subtractionResult = 10 - 2;
+console.log("10 - 2 = ".concat(subtractionResult));
+// Multiplication operation
+var multiplicationResult = 4 * 2;
+console.log("4 * 2 = ".concat(multiplicationResult));
+// Division operation
+var divisionResult = 16 / 2;
+console.log("16 / 2 = ".concat(divisionResult));
---
+++ Question7/Q7.ts
@@ -0,0 +1,15 @@
+// Addition operation
+const additionResult: number = 5 + 3;
+console.log(`5 + 3 = ${additionResult}`);
+
+// Subtraction operation
+const subtractionResult: number = 10 - 2;
+console.log(`10 - 2 = ${subtractionResult}`);
+
+// Multiplication operation
+const multiplicationResult: number = 4 * 2;
+console.log(`4 * 2 = ${multiplicationResult}`);
+
+// Division operation
+const divisionResult: number = 16 / 2;
+console.log(`16 / 2 = ${divisionResult}`); 
