41075327985b20b237b53b93c6c4017b86a5bca3
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-17 19:57:10 +0500

    Add files via upload

---
+++ Questtion13/Q13.ts
@@ -0,0 +1,4 @@
+const transportation_names: string[] = ["Mercedes, Ferrari, Buggati, Streetfighter V4 S"]
+for (let _name of transportation_names) {
+    console.log(`I would love to own a ${transportation_names}`);
+}
