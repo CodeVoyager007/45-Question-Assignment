35d9e8fef8f681c98a7ee05ce0825a63b4dcb366
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-17 16:57:20 +0500

    Add files via upload

---
+++ Question3/Q3.js
@@ -0,0 +1,4 @@
+var personName2 = "Ayesha Mughal";
+console.log(personName2.toLowerCase());
+console.log(personName2.toUpperCase());
+console.log(personName2.charAt(0).toUpperCase() + personName2.slice(1).toLowerCase());
---
+++ Question3/Q3.ts
@@ -0,0 +1,4 @@
+let personName2: string = "Ayesha Mughal";
+console.log(personName2.toLowerCase());
+console.log(personName2.toUpperCase());
+console.log(personName2.charAt(0).toUpperCase() + personName2.slice(1).toLowerCase());
