2dd83ac00c6e65a828b09c976164b49e30ab2219
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-17 17:00:15 +0500

    Add files via upload

---
+++ Question5/Q5.js
@@ -0,0 +1,3 @@
+var famous_person = "Steve Jobs";
+var message = "".concat(famous_person, " once said, \"The only way to do great work is to love what you do\" - Steve Jobs..\"");
+console.log(message);
