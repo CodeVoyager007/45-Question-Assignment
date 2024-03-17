9344bcd2f7194321588ac690367aafb55e4ab9f8
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-18 02:31:19 +0500

    Delete Questtion13 directory

--- Questtion13/Q13.js
+++
@@ -1,5 +0,0 @@
-var transportation_names = ["Mercedes, Ferrari, Buggati, Streetfighter V4 S"];
-for (var _i = 0, transportation_names_1 = transportation_names; _i < transportation_names_1.length; _i++) {
-    var _name = transportation_names_1[_i];
-    console.log("I would love to own a ".concat(transportation_names));
-}
