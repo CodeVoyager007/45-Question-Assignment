cfbcca25e14208139d3bcc170d60ff108a162396
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-17 19:56:12 +0500

    Add files via upload

---
+++ Question12/Q12.js
@@ -0,0 +1,6 @@
+var friends_names = ["Amna", "Maheen", "Nimra"];
+// Greetings
+for (var _i = 0, friends_names_1 = friends_names; _i < friends_names_1.length; _i++) {
+    var name_1 = friends_names_1[_i];
+    console.log("Assalam-o-alikum, ".concat(name_1, "! Have a wonderful."));
+}
---
+++ Question12/Q12.ts
@@ -0,0 +1,6 @@
+const friends_names: string[] = ["Amna", "Maheen", "Nimra"];
+
+// Greetings
+for (let name of friends_names) {
+    console.log(`Assalam-o-alikum, ${name}! Have a wonderful.`);
+}
