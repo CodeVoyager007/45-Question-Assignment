9b3c58cf24c880079a56c97a94cac2e033db1b00
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-17 16:58:55 +0500

    Add files via upload

---
+++ Question4/Q4.js
@@ -0,0 +1,3 @@
+var quote = "To succeed in your mission, you must have single-minded devotion to your goal.";
+var author = "APJ Abdul Kalam";
+console.log("".concat(author, " once said, \"").concat(quote, "\""));
---
+++ Question4/Q4.ts
@@ -0,0 +1,3 @@
+let quote: string = "To succeed in your mission, you must have single-minded devotion to your goal.";
+let author: string = "APJ Abdul Kalam";
+console.log(`${author} once said, "${quote}"`);
