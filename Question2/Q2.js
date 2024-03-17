efcf0ea8b88c6cce2c64c6915d525a65f0ea0956
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-17 16:50:29 +0500

    Add files via upload

---
+++ Question2/Q2.js
@@ -0,0 +1,2 @@
+var personName = "Ayesha Mughal";
+console.log("Hello ".concat(personName, ", wanna have a tea party at my palce at 6:00?"));
---
+++ Question2/Q2.ts
@@ -0,0 +1,2 @@
+let personName: string = "Ayesha Mughal";
+console.log(`Hello ${personName}, wanna have a tea party at my palce at 6:00?`);
