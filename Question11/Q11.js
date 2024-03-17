b86ae603651f88f47f5dc5baedbd1ebeac0adc3b
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-17 19:54:43 +0500

    Add files via upload

---
+++ Question11/Q11.js
@@ -0,0 +1,5 @@
+var friends_name = ["Amna", "Maheen", "Nimra"];
+for (var _i = 0, friends_name_1 = friends_name; _i < friends_name_1.length; _i++) {
+    var name_1 = friends_name_1[_i];
+    console.log(name_1);
+}
---
+++ Question11/Q11.ts
@@ -0,0 +1,3 @@
+const friends_name: string[] = ["Amna", "Maheen", "Nimra"];
+for (let name of friends_name) {
+    console.log(name);}
