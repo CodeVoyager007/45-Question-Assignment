25c80fe2ac1dbd32477036e9d8bbf349c5f5cdb0
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-18 00:16:17 +0500

    Add files via upload

---
+++ Question18/Q18.js
@@ -0,0 +1,5 @@
+var countries = ["USA", "Canada", "Japan", "Italy", "North Korea"];
+console.log("List of Countries:");
+countries.forEach(function (country) {
+    console.log(country);
+});
---
+++ Question18/Q18.ts
@@ -0,0 +1,5 @@
+let countries: string[] = ["USA", "Canada", "Japan", "Italy", "North Korea"];
+console.log("List of Countries:");
+countries.forEach((country) => {
+    console.log(country);
+});
