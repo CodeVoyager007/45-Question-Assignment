4f6d9fcdb41274051128239b73e3f1432acb9d12
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-18 09:58:28 +0500

    Add files via upload

---
+++ Question27/Q27.js
@@ -0,0 +1,30 @@
+var alien_color = 'blue';
+if (alien_color === 'green') {
+    console.log("You just earned 5 points for shooting the green alien!");
+}
+else if (alien_color === 'yellow') {
+    console.log("You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color === 'red') {
+    console.log("You just earned 15 points for shooting the red alien.");
+}
+alien_color = 'orange';
+if (alien_color === 'green') {
+    console.log("You just earned 5 points for shooting the green alien!");
+}
+else if (alien_color === 'yellow') {
+    console.log("You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color === 'red') {
+    console.log("You just earned 15 points for shooting the red alien.");
+}
+alien_color = 'purple';
+if (alien_color === 'green') {
+    console.log("You just earned 5 points for shooting the green alien!");
+}
+else if (alien_color === 'yellow') {
+    console.log("You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color === 'red') {
+    console.log("You just earned 15 points for shooting the red alien.");
+}
