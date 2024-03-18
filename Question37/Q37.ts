032e01ece68ac86cf96adad42be838daeff169ac
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-18 20:12:29 +0500

    Add files via upload

---
+++ Question37/Q37.ts
@@ -0,0 +1,6 @@
+function tailorShirt(type: string = "large", text: string = "I Love TypeScript") {
+    console.log(`Crafting a ${type} t-shirt with the phrase "${text}" imprinted.`);
+}
+tailorShirt(); 
+tailorShirt("medium"); 
+tailorShirt("small", "Embrace Coding"); 
