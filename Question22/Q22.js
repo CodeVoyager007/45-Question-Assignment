387b9a9b06543ef4f6eee1052a399a3005edc74b
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-18 09:15:06 +0500

    Add files via upload

---
+++ Question22/Q22.js
@@ -0,0 +1,4 @@
+var colors = ["red", "green", "blue"];
+console.log(colors[3]); // Intentional error
+console.log(colors[1]); // Corrected
+console.log("Colors:", colors);
---
+++ Question22/Q22.ts
@@ -0,0 +1,7 @@
+let colors: string[] = ["red", "green", "blue"];
+
+console.log(colors[3]); // Intentional error
+
+console.log(colors[1]); // Corrected
+
+console.log("Colors:", colors);
