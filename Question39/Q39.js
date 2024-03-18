1a8be208347480776d10476720227ea97bc363d1
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-18 20:14:56 +0500

    Add files via upload

---
+++ Question39/Q39.js
@@ -0,0 +1,6 @@
+function combineCityAndCountry(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+console.log(combineCityAndCountry("Lahore", "Pakistan"));
+console.log(combineCityAndCountry("Melbourne", "Australia"));
+console.log(combineCityAndCountry("Tokyo", "Japan"));
---
+++ Question39/Q39.ts
@@ -0,0 +1,6 @@
+function combineCityAndCountry(city: string, country: string): string {
+    return `${city}, ${country}`;
+}
+console.log(combineCityAndCountry("Lahore", "Pakistan"));
+console.log(combineCityAndCountry("Melbourne" , "Australia")); 
+console.log(combineCityAndCountry("Tokyo", "Japan"));
