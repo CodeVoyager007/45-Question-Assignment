ee3364cc72d0c0926eaf5cfcdd80c08cbbcd190d
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-18 09:16:14 +0500

    Add files via upload

---
+++ Question23/Q23.js
@@ -0,0 +1,16 @@
+var car = 'subaru';
+console.log(car.toLowerCase() == 'subaru');
+console.log(car == 'subaru');
+console.log(10 >= 10);
+console.log(car != 'ford');
+console.log(5 < 3);
+console.log(car != 'subaru');
+console.log(7 <= 6);
+console.log(car == 'toyota');
+console.log(car.toLowerCase() == 'SUBARU');
+var favoriteCities = ['Paris', 'Tokyo', 'New York', 'London'];
+var cityToCheck = 'Tokyo';
+console.log(favoriteCities.includes(cityToCheck));
+var spokenLanguages = ['English', 'Spanish', 'French', 'Mandarin'];
+var languageToCheck = 'German';
+console.log(spokenLanguages.includes(languageToCheck));
---
+++ Question23/Q23.ts
@@ -0,0 +1,22 @@
+let car: string = 'subaru';
+
+console.log(car.toLowerCase() == 'subaru');
+console.log(car == 'subaru');
+console.log(10 >= 10);
+console.log(car != 'ford');
+console.log(5 < 3);
+console.log(car != 'subaru');
+console.log(7 <= 6);
+console.log(car == 'toyota');
+console.log(car.toLowerCase() == 'SUBARU');
+
+let favoriteCities: string[] = ['Paris', 'Tokyo', 'New York', 'London'];
+let cityToCheck: string = 'Tokyo';
+
+console.log(favoriteCities.includes(cityToCheck));
+
+let spokenLanguages: string[] = ['English', 'Spanish', 'French', 'Mandarin'];
+let languageToCheck: string = 'German';
+
+console.log(spokenLanguages.includes(languageToCheck));
+
