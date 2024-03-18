4458ae6bbb850da004025b58c5c0f684611fefad
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-18 20:11:31 +0500

    Add files via upload

---
+++ Question36/Q36.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    console.log("T-shirt size: ".concat(size.toUpperCase()));
+}
+make_shirt("Medium", "Keep Coding!");
---
+++ Question36/Q36.ts
@@ -0,0 +1,4 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`T-shirt size: ${size.toUpperCase()}`);
+}
+make_shirt("Medium", "Keep Coding!");
