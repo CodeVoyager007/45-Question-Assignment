6888e62d3bb726d3ef1cb64ce7dbbbcf56926c02
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-18 09:18:11 +0500

    Add files via upload

---
+++ Question24/Q24.js
@@ -0,0 +1,42 @@
+// Tests for equality and inequality with strings
+var string1 = "hamburger";
+var string2 = "pizza";
+console.log("Is string1 equal to string2?");
+console.log(string1 === string2); // False
+console.log("Is string1 not equal to string2?");
+console.log(string1 !== string2); // True
+// Tests using the lower case function
+var mixedCaseString = "French Fries are delicious!";
+console.log("Is mixedCaseString equal to 'french fries are delicious!'?");
+console.log(mixedCaseString.toLowerCase() === "french fries are delicious!"); // True
+console.log("Is mixedCaseString equal to 'French Fries are delicious!'?");
+console.log(mixedCaseString.toLowerCase() === "French Fries are delicious!"); // False
+// Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+var num1 = 15;
+var num2 = 7;
+console.log("Is num1 equal to num2?");
+console.log(num1 === num2); // False
+console.log("Is num1 not equal to num2?");
+console.log(num1 !== num2); // True
+console.log("Is num1 greater than num2?");
+console.log(num1 > num2); // True
+console.log("Is num1 less than num2?");
+console.log(num1 < num2); // False
+console.log("Is num1 greater than or equal to num2?");
+console.log(num1 >= num2); // True
+console.log("Is num1 less than or equal to num2?");
+console.log(num1 <= num2); // False
+// Tests using "and" and "or" operators
+var condition1 = true;
+var condition2 = false;
+console.log("Is condition1 true and condition2 false?");
+console.log(condition1 && condition2); // False
+console.log("Is condition1 true or condition2 false?");
+console.log(condition1 || condition2); // True
+// Test whether an item is in an array
+// Test whether an item is not in an array
+var items = ["Burger", "Hot Dog", "French Fries", "Pizza"];
+console.log("Is 'Burger' included in the items array?");
+console.log(items.includes("Burger")); // True
+console.log("Is 'Sushi' included in the items array?");
+console.log(items.includes("Sushi")); // False
