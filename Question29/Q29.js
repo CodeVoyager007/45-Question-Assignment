b8a4f8db6f24c6c2e690b4a88d8f9e981395ba51
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-03-18 10:00:41 +0500

    Add files via upload

---
+++ Question29/Q29.js
@@ -0,0 +1,16 @@
+var favorite_fruits = ["Mangoes", "Grapes", "strawberry"];
+if (favorite_fruits.includes("Mangoes")) {
+    console.log("You really like Grapes!");
+}
+if (favorite_fruits.includes("Grapes")) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.includes("strawberry")) {
+    console.log("You really like strawberries!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like oranges!");
+}
+if (favorite_fruits.includes("kiwi")) {
+    console.log("You really like kiwis!");
+}
---
+++ Question29/Q29.ts
@@ -0,0 +1,17 @@
+const favorite_fruits: string[] = ["Mangoes", "Grapes", "strawberry"];
+
+if (favorite_fruits.includes("Mangoes")) {
+    console.log("You really like Grapes!");
+}
+if (favorite_fruits.includes("Grapes")) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.includes("strawberry")) {
+    console.log("You really like strawberries!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like oranges!");
+}
+if (favorite_fruits.includes("kiwi")) {
+    console.log("You really like kiwis!");
+}
